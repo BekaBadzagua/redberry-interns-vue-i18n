187af68766008465b929f1b9ff190c0a014c1753
bekabadzagua <beka.badzagua@gmail.com>
2022-11-08 14:48:45 +0400

    feat: integrate i18n

---
+++ src/i18n/lang/en/index.js
@@ -0,0 +1,15 @@
+import advantages2 from "./advantages-v2.json";
+import advantages from "./advantages.json";
+import car from "./car.json";
+import filter from "./filter.json";
+import texts from "./texts.json";
+
+const translations = {
+  texts,
+  advantages2,
+  advantages,
+  car,
+  filter,
+};
+
+export default translations;
